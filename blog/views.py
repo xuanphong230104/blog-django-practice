3ef5953289138e4e0117e1a8ccbfa43f1d976622
xuanphong230104 <nguyenxuanphong230104@gmail.com>
2024-09-20 16:20:48 +0700

    first commit

---
+++ blog/views.py
@@ -0,0 +1,10 @@
+from django.views.generic import ListView,DetailView
+from .models import Post
+# Create your views here.
+class BlogListView(ListView):
+    model = Post
+    template_name = "home.html"
+
+class BlogDetailView(DetailView): # new
+    model = Post
+    template_name = "post_detail.html"
